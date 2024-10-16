91714197e5f12101878964092f14479f8f94826b
OliverUv <oliver.uvman@gmail.com>
2024-10-16 19:40:41 +0200

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Command {
-    Show{recipe: String},
+    Calc{recipe: String},
     // List,
 }
 
@@ -25,15 +25,103 @@ fn main() -> Result<(), anyhow::Error> {
 
     let cli = Cli::parse();
     match &cli.command {
-        Command::Show{recipe} => show(state, all_recipes, recipe.as_str())?,
+        Command::Calc{recipe} => calc(state, all_recipes, recipe.as_str())?,
     }
 
     Ok(())
 }
 
-fn show(state: State, all_recipes: RecipeMap, recipe: &str) -> Result<(), anyhow::Error> {
-    let r = all_recipes.get(recipe)
-        .ok_or(anyhow!("No such recipe: {}", &recipe))?;
+fn calc(state: State, all_recipes: RecipeMap, recipe: &str) -> Result<(), anyhow::Error> {
+    let mut key = None;
+    for k in all_recipes.keys() {
+        let kl = k.as_str().to_lowercase();
+        let search_key = recipe.to_lowercase();
+        if kl == search_key { key = Some(k.clone()); }
+    }
+    let k = key.ok_or(anyhow!("No such recipe: {}", &recipe))?;
+    let r = all_recipes.get(&k).ok_or(anyhow!("Could not find recipe: {k}"))?;
     r.print_calc(&state)?;
     Ok(())
 }
+
+impl Recipe {
+    pub fn print_calc(&self, state: &State) -> anyhow::Result<()> {
+        let (max_belt, max_pipe) = self.max_outputs();
+        let RecipeCalc {
+            use_belt,
+            use_pipe,
+            m_per_belt,
+            m_per_pipe,
+            n_boxes,
+            pref_mult,
+            clock,
+        } = self.calc(state)?;
+
+        let in_out_modifier = clock * n_boxes * pref_mult;
+
+        println!("\n{:12}{:>39}", self.building, self.name);
+        println!("\n  --  IN  --");
+        print_ingredient(&self.in_1, None);
+        print_ingredient(&self.in_2, None);
+        print_ingredient(&self.in_3, None);
+        print_ingredient(&self.in_4, None);
+        println!("\n  -- OUT  --");
+        print_ingredient(&self.out_1, None);
+        print_ingredient(&self.out_2, None);
+        println!("\n  -- CALC --");
+
+        if use_belt {
+            println!("Max belt use: {:8}", max_belt);
+        }
+        if use_pipe {
+            println!("Max pipe use: {:8}", max_pipe);
+        }
+        if use_belt {
+            println!(
+                "Num of {} per belt: {:8.4}",
+                &self.building,
+                m_per_belt,
+            );
+        }
+        if use_pipe {
+            println!(
+                "Num of {} per pipe: {:8.4}",
+                &self.building,
+                m_per_pipe,
+            );
+        }
+
+        println!("\n  --  BP  --");
+        println!("{} [{:.0}]", self.name, n_boxes);
+        println!("Num {} per instance: {}", self.building, pref_mult);
+        println!("Clock: {:5.2} %", clock * 100.0);
+        println!("Out:");
+        print_ingredient(&self.out_1, Some(in_out_modifier));
+        print_ingredient(&self.out_2, Some(in_out_modifier));
+        println!("In:");
+        print_ingredient(&self.in_1, Some(in_out_modifier));
+        print_ingredient(&self.in_2, Some(in_out_modifier));
+        print_ingredient(&self.in_3, Some(in_out_modifier));
+        print_ingredient(&self.in_4, Some(in_out_modifier));
+
+        Ok(())
+    }
+
+}
+
+fn print_ingredient(i: &Option<Ingredient>, modify: Option<f64>) {
+    let i = match i {
+        Some(i) => i,
+        None => return,
+    };
+    let t = match i.transport() {
+        Transport::Belt => "Belt",
+        Transport::Pipe => "Pipe",
+    };
+    match modify {
+        None => println!("({:4})  {:27} {:15.4}", t, i.part, i.quantity),
+        Some(m) => println!("  {:24} {:7.2}", i.part, m * i.quantity),
+    }
+    
+}
+
--- src/types.rs
+++ src/types.rs
@@ -27,7 +27,7 @@ impl Default for State {
         Self {
             belt_ipm: 480.0,
             pipe_ipm: 600.0,
-            pref_multiple_constructor: 2.0,
+            pref_multiple_constructor: 3.0,
             pref_multiple_assembler: 3.0,
             pref_multiple_manufacturer: 2.0,
             pref_multiple_refinery: 4.0,
@@ -74,79 +74,6 @@ pub struct Recipe {
 }
 
 impl Recipe {
-    pub fn print_calc(&self, state: &State) -> anyhow::Result<()> {
-        println!("\n{:12}{:>39}", self.building, self.name);
-        println!("\n  --  IN  --");
-        print_ingredient(&self.in_1, None);
-        print_ingredient(&self.in_2, None);
-        print_ingredient(&self.in_3, None);
-        print_ingredient(&self.in_4, None);
-        println!("\n  -- OUT  --");
-        print_ingredient(&self.out_1, None);
-        print_ingredient(&self.out_2, None);
-        println!("\n  -- CALC --");
-        let (max_belt, max_pipe) = self.max_outputs();
-        let use_belt = max_belt >= 0.00001;
-        let use_pipe = max_pipe >= 0.00001;
-        let m_per_belt = state.belt_ipm / max_belt;
-        let m_per_pipe = state.pipe_ipm / max_pipe;
-        let m_per_transport = if use_belt && use_pipe {
-            m_per_belt.min(m_per_pipe)
-        } else if use_belt {
-            m_per_belt
-        } else {
-            m_per_pipe
-        };
-
-        let pref_mult = state.prefered_building_multiple(self.building.as_str()).ok_or(anyhow!("Please state a prefered number of machines for {}", &self.building))?;
-        let mut n_boxes = m_per_transport / pref_mult;
-        let mut clock = 1.0;
-        if n_boxes.fract().abs() > 0.0001 {
-            // need to +1 the amount of boxes and adjust clocks
-            let n_boxes_adjusted = n_boxes.ceil();
-            clock = n_boxes / n_boxes_adjusted;
-            n_boxes = n_boxes_adjusted;
-        }
-        let in_out_modifier = clock * n_boxes * pref_mult;
-        clock *= 100.0;
-
-        if use_belt {
-            println!("Max belt use: {:8}", max_belt);
-        }
-        if use_pipe {
-            println!("Max pipe use: {:8}", max_pipe);
-        }
-        if use_belt {
-            println!(
-                "Num of {} per belt: {:8.4}",
-                &self.building,
-                m_per_belt,
-            );
-        }
-        if use_pipe {
-            println!(
-                "Num of {} per pipe: {:8.4}",
-                &self.building,
-                m_per_pipe,
-            );
-        }
-
-        println!("\n  --  BP  --");
-        println!("{} [{:.0}]", self.name, n_boxes);
-        println!("Num {} per instance: {}", self.building, pref_mult);
-        println!("Clock: {:5.2} %", clock);
-        println!("Out:");
-        print_ingredient(&self.out_1, Some(in_out_modifier));
-        print_ingredient(&self.out_2, Some(in_out_modifier));
-        println!("In:");
-        print_ingredient(&self.in_1, Some(in_out_modifier));
-        print_ingredient(&self.in_2, Some(in_out_modifier));
-        print_ingredient(&self.in_3, Some(in_out_modifier));
-        print_ingredient(&self.in_4, Some(in_out_modifier));
-
-        Ok(())
-    }
-
     pub fn max_outputs(&self) -> (f64, f64) {
         let mut belt = 0.0;
         let mut pipe = 0.0;
@@ -172,22 +99,51 @@ impl Recipe {
 
         (belt, pipe)
     }
+
+    pub fn calc(&self, state: &State) -> anyhow::Result<RecipeCalc> {
+        let (max_belt, max_pipe) = self.max_outputs();
+        let use_belt = max_belt >= 0.00001;
+        let use_pipe = max_pipe >= 0.00001;
+        let m_per_belt = state.belt_ipm / max_belt;
+        let m_per_pipe = state.pipe_ipm / max_pipe;
+        let m_per_transport = if use_belt && use_pipe {
+            m_per_belt.min(m_per_pipe)
+        } else if use_belt {
+            m_per_belt
+        } else {
+            m_per_pipe
+        };
+
+        let pref_mult = state.prefered_building_multiple(self.building.as_str()).ok_or(anyhow!("Please state a prefered number of machines for {}", &self.building))?;
+        let mut n_boxes = m_per_transport / pref_mult;
+        let mut clock = 1.0;
+        if n_boxes.fract().abs() > 0.0001 {
+            // need to +1 the amount of boxes and adjust clocks
+            let n_boxes_adjusted = n_boxes.ceil();
+            clock = n_boxes / n_boxes_adjusted;
+            n_boxes = n_boxes_adjusted;
+        }
+
+        Ok(RecipeCalc {
+            use_belt,
+            use_pipe,
+            m_per_belt,
+            m_per_pipe,
+            n_boxes,
+            pref_mult,
+            clock,
+        })
+    }
 }
 
-fn print_ingredient(i: &Option<Ingredient>, modify: Option<f64>) {
-    let i = match i {
-        Some(i) => i,
-        None => return,
-    };
-    let t = match i.transport() {
-        Transport::Belt => "Belt",
-        Transport::Pipe => "Pipe",
-    };
-    match modify {
-        None => println!("({:4})  {:27} {:15.4}", t, i.part, i.quantity),
-        Some(m) => println!("  {:24} {:7.2}", i.part, m * i.quantity),
-    }
-    
+pub struct RecipeCalc {
+    pub use_belt: bool,
+    pub use_pipe: bool,
+    pub m_per_belt: f64,
+    pub m_per_pipe: f64,
+    pub n_boxes: f64,
+    pub pref_mult: f64,
+    pub clock: f64,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
